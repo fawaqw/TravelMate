a1c1062e55ee253b90a3acfc283c254415d199ac
fawaqw <darya20060512@gmail.com>
2026-02-11 22:37:11 +0500

    Changes for successful build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,23 +21,25 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "com.example.travelmate.HiltTestRunner"
     }
 
-    buildFeatures {
-        compose = true
-    }
+    buildFeatures { compose = true }
 
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.10"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
-
 }
+
 
 dependencies {
     // Compose
